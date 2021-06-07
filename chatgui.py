f24e0e72f1cf45da1ff8a78065406629343c0d65
Phila-Dyantyi <68436241+Phila-Dyantyi@users.noreply.github.com>
2021-06-07 21:45:34 +0200

    Add files via upload

--- chatgui.py
+++ chatgui.py
@@ -84,35 +84,43 @@ def send():
         ChatLog.config(state=DISABLED)
         ChatLog.yview(END)
  
+userinput = ''
+print('Hello! Welcome to tweet bot')
+print("type \'\\exit\' to terminate application")
+while userinput != '\exit':
+    userinput = input('')
+    print('user: ', userinput)
+    res = chatbot_response(userinput)
+    print('bot: ', res)
 
-base = Tk()
-base.title("Hello! Welcome to tweet bot")
-base.geometry("400x500")
-base.resizable(width=FALSE, height=FALSE)
+# base = Tk()
+# base.title("Hello! Welcome to tweet bot")
+# base.geometry("400x500")
+# base.resizable(width=FALSE, height=FALSE)
 
-#Create Chat window
-ChatLog = Text(base, bd=0, bg="grey", height="8", width="50", font="Arial",)
+# #Create Chat window
+# ChatLog = Text(base, bd=0, bg="grey", height="8", width="50", font="Arial",)
 
-ChatLog.config(state=DISABLED)
+# ChatLog.config(state=DISABLED)
 
-#Bind scrollbar to Chat window
-scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-ChatLog['yscrollcommand'] = scrollbar.set
+# #Bind scrollbar to Chat window
+# scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+# ChatLog['yscrollcommand'] = scrollbar.set
 
-#Create Button to send message
-SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
-                    command= send )
+# #Create Button to send message
+# SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+#                     bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+#                     command= send )
 
-#Create the box to enter message
-EntryBox = Text(base, bd=0, bg="grey",width="29", height="5", font="Arial")
-#EntryBox.bind("<Return>", send)
+# #Create the box to enter message
+# EntryBox = Text(base, bd=0, bg="grey",width="29", height="5", font="Arial")
+# #EntryBox.bind("<Return>", send)
 
 
-#Place all components on the screen
-scrollbar.place(x=376,y=6, height=386)
-ChatLog.place(x=6,y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+# #Place all components on the screen
+# scrollbar.place(x=376,y=6, height=386)
+# ChatLog.place(x=6,y=6, height=386, width=370)
+# EntryBox.place(x=128, y=401, height=90, width=265)
+# SendButton.place(x=6, y=401, height=90)
 
-base.mainloop()
+# base.mainloop()
